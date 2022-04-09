c35e0d27c863999abf170dcfd711a705ff14111c
BANKIEYZ\tanat <bank250145.bb@gmail.com>
2022-04-09 10:32:19 +0700

    use loop

--- src/Pages/MovieList.tsx
+++ src/Pages/MovieList.tsx
@@ -1,25 +1,18 @@
-import React from 'react'
-import MovieCom from '../components/MovieCom'
+import React from "react";
+import MovieCom from "../components/MovieCom";
 
 function MovieList() {
+  const Movies: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
     <div>
-      <div className='grid grid-cols-3 items-center justify-items-center gap-10  '>
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
-        <MovieCom />
+      <div className="grid grid-cols-3 items-center justify-items-center gap-10  ">
+        {Movies.map((movie) => (
+            <MovieCom key={movie} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
