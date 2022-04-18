b695da94ab26e13bbec438bfa8c6731d70bdc465
TanatornZ <bank250145.bb@gmail.com>
2022-04-18 16:59:26 +0700

    update 18

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      spacing: {
-        "8xl": "90px",
-        "9xl": "148px",
-        "10xl": "280px",
-        "11xl": "380px",
-        "12xl": "480px",
-        modal: "600px",
+  module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          nunito: ["Nunito", "sans-serif"],
+        },
+        screens: {
+          desktop: "1440px",
+        },
+        spacing: {
+          "8xl": "90px",
+          "9xl": "148px",
+          "10xl": "280px",
+          "11xl": "380px",
+          "12xl": "480px",
+          modal: "600px",
+        },
+        colors: {
+          "dark-opacity": "rgba(20,20,20,0.5)",
+        },
       },
-    colors: {
-      "dark-opacity": "rgba(20,20,20,0.5)",
     },
-    },
-  },
-  plugins: [],
-};
+    plugins: [],
+  };
